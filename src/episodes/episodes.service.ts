60bd491f12c322acf0c8081e81eed8f99cfbfe46
princechrix <princechrix.dev@gmail.com>
2025-01-05 23:28:40 +0200

    ''

---
+++ src/episodes/episodes.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+
+export class Episode {
+  id: string;
+  name: string;
+  featured?: boolean;
+}
+
+export class CreateEpisodeDto {
+  name: string;
+  featured?: boolean;
+}
+
+@Injectable()
+export class EpisodesService {
+  private episodes: Episode[] = [];
+
+  async findAll(sort: 'asc' | 'desc' = 'desc') {
+    const sortAsc = (a: Episode, b: Episode) => (a.name > b.name ? 1 : -1);
+    const sortDesc = (a: Episode, b: Episode) => (a.name < b.name ? 1 : -1);
+
+    return sort === 'asc'
+      ? this.episodes.sort(sortAsc)
+      : this.episodes.sort(sortDesc);
+  }
+
+  async findFeatured() {
+    return this.episodes.filter((episode) => episode.featured);
+  }
+
+  async findOne(id: string) {
+    return this.episodes.find((episode) => episode.id === id);
+  }
+
+  async create(createEpisodeDto: CreateEpisodeDto) {
+    const newEpisode = { ...createEpisodeDto, id: randomUUID() };
+    this.episodes.push(newEpisode);
+    return newEpisode;
+  }
+}
