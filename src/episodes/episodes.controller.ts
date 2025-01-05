f5c8162149338f6fa1af6100c200eb4ff99617f3
princechrix <princechrix.dev@gmail.com>
2025-01-05 23:14:35 +0200

    ''

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('episodes')
 export class EpisodesController {
   @Get()
-  findAll() {
+  findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
+    console.log(sort);
     return 'All Episodes';
   }
 
+  @Get('featured')
+  findFeatured() {
+    return 'featured episodes';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    console.log(id); 
+    return 'One Episode ' + id;
+  }
+
   @Post()
-  create() {
+  create(@Body() input: any) {
+    console.log(input);
     return 'new Episode';
   }
 }
